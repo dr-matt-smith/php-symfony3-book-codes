7055744c3d9d272413dd40b2f95b283e46468c3b
dr-matt-smith <dr_matt_smith@me.com>
2017-03-07 09:55:12 +0000

    part05 sessions draft complete

--- part05/project14/src/AppBundle/Controller/DefaultController.php
+++ part05/project14/src/AppBundle/Controller/DefaultController.php
@@ -10,9 +10,6 @@ use Symfony\Component\HttpFoundation\Request;
 
 use Symfony\Component\HttpFoundation\Response;
 
-use Symfony\Component\HttpFoundation\Session\Session;
-
-
 
 class DefaultController extends Controller
 {
@@ -21,30 +18,12 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // create colours array
-        $colours = [
-            'foreground' => 'blue',
-            'background' => 'pink'
-        ];
-
-        // store colours in session 'colours'
-        $session = new Session();
-        $session->set('colours', $colours);
-
         $argsArray =  [
             'name' => 'matt'
         ];
 
         $templateName = 'index';
         return $this->render($templateName . '.html.twig', $argsArray);
-    }
-
-    /**
-     * @Route("/pop", name="pop")
-     */
-    public function popAction(Request $request)
-    {
-        return new Response('pop goes Symfony');
     }
 
 }
